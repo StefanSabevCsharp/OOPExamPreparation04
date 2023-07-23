ed87099c4a34d50ef856b7eced7f4da6d8dcf137
Stefan Sabev <stevenbg91@gmail.com>
2023-07-23 21:56:33 +0200

    Implementing Repositories

---
+++ Models/Hotels/Hotel.cs
@@ -0,0 +1,61 @@
+﻿using BookingApp.Models.Bookings;
+using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories.Contracts;
+using BookingApp.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models.Hotels
+{
+    public class Hotel : IHotel
+    {
+        private string fullName;
+        private int category;
+       
+
+        public Hotel(string fullName, int category)
+        {
+            this.fullName = fullName;
+            this.category = category;
+           
+        }
+
+        public string FullName
+        {
+            get => fullName;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.HotelNameNullOrEmpty);
+                }
+                fullName = value;
+            }
+        }
+
+        public int Category
+        {
+            get => category;
+            private set
+            {
+                if (value < 1 || value > 5)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCategory);
+                }
+                category = value;
+            }
+        }
+
+        public double Turnover { get; private set; }
+ 
+
+        public IRepository<IRoom> Rooms{ get;}
+
+        public IRepository<IBooking> Bookings { get;}
+    }
+}
